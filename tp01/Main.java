343b1c8200452a385dec8645297057459cca4716
Fernanda Mariano <121069175+fermariano@users.noreply.github.com>
2024-02-21 19:03:06 -0300

    converte em arq binario

---
+++ tp01/Main.java
@@ -0,0 +1,70 @@
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String[] lerArq(String path) {
+        File file = new File(path); // le o arquivo
+        String[] arrData = new String[33000]; // array com linha por linha do arquivo
+        int i = 0; 
+        try {
+            Scanner scanner = new Scanner(file); 
+            scanner.nextLine(); 
+            while (scanner.hasNext()) {
+                arrData[i] = scanner.nextLine(); // le linha por linha e salva no array
+                i++;
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+        }
+        return arrData;
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] arrData = lerArq("spotSongs.csv"); // le o arquivo e cada posiçao é uma linha
+        Musica[] musicaData = new Musica[arrData.length]; // array com todos os jogadores
+        
+        for (int i = 0; i < arrData.length && arrData[i] != null; i++) {
+            
+                String[] atributos = arrData[i].split(",");
+                System.out.println("------");
+                for (String a : atributos) System.out.println(a);
+                    System.out.println("------");
+                    musicaData[i] = new Musica(atributos[0], atributos[1], Integer.parseInt(atributos[2]), atributos[3], atributos[4], Float.parseFloat(atributos[5]));
+             
+               
+            
+            }
+        
+
+        FileOutputStream arq;
+        DataOutputStream dos;
+
+            
+        try {
+            arq = new FileOutputStream("songs.db");
+            dos = new DataOutputStream(arq);
+
+            for (int i = 0; i < musicaData.length && musicaData[i]!= null; i++) {
+                dos.writeUTF(musicaData[i].nome);
+                dos.writeUTF(musicaData[i].artista);
+                dos.writeLong(musicaData[i].date);
+                dos.writeUTF(musicaData[i].genero[0]);
+                dos.writeUTF(musicaData[i].genero[1]);
+                dos.writeFloat(musicaData[i].dance);
+                System.out.println(musicaData[i].toString());
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+}
---
+++ tp01/Musica.java
@@ -0,0 +1,83 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Musica {
+    static int idCount = 0; //contador de id
+    int id; 
+    String nome; 
+    String artista; 
+    int popular;
+    long date;
+    String[] genero; //vairos generos (2)
+    float dance;
+
+    public Musica() {
+        this.id = idCount++; //preenche o ID dinamicamente
+        this.nome = "Vazio";
+        this.artista = "Nenhum"; //nome do artista limitado
+        this.popular = 0;
+        this.date = 0;
+        this.dance = 0;
+        this.genero = iniciarGenero();
+    }
+
+
+    public Musica(String nome, String artista, int popular, String date, String genero, float dance) {
+        this.id = idCount++;
+        this.nome = nome;
+        this.artista = artista;
+        this.popular = popular;
+        try {
+            this.date = dataConverter(date);
+        } catch (Exception e) {
+            try{
+                this.date = dataConverter(date+"-11-22"); //<3
+            }catch(Exception e2){ //desisto
+                this.date = dataConverter(date+"-22");
+            }
+            
+        }
+        this.genero = genero.split(";");
+        this.dance = dance;
+    }
+
+    private static String[] iniciarGenero(){
+        String genero [] = new String[2];
+        genero[0] = "";
+        genero[1]= "";
+        return genero;
+    } 
+
+    // converte a data para timestamp
+    private long dataConverter(String data) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // define o formato da data
+        LocalDate localDate = LocalDate.parse(data, formatter); // converte string para localdate
+        Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant(); // converte localdate para instant
+        return instant.toEpochMilli(); // retorna timestamp
+    }
+
+    // converte timestamp para data
+    private String timestampConv(long timestamp) {
+        Instant instant = Instant.ofEpochMilli(timestamp); // converte timestamp para instant
+        
+        LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate(); // converte instant para localdate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // formata a data
+        return localDate.format(formatter);
+    }
+
+    public String toString() {
+        return "\nID: " + id +
+        "\nNome: " + nome +
+        "\nArtista: " + artista + 
+        "\nPopular: " + popular + 
+        "\nDate: " + timestampConv(date) + 
+        "\nDance: " + dance +
+        "\nGenero: " + genero[0] + ", " + genero[1]; 
+    }
+
+    
+}
