b1fdb0cac0810aa1cacfe607f30bc9b070c756f4
fermariano <fernandamariano202@gmail.com>
2024-02-25 19:30:25 -0300

    add comentarios

--- tp01/src/main/java/Arq.java
+++ tp01/src/main/java/Arq.java
@@ -9,22 +9,21 @@ public class Arq {
     /**
      * InnerArq
      */
+
+     // procura o registro no arquivo
     protected static class DataFinder {
         int ID;
         long seekSaver;
         long metaSeek;
         MetaData metaData;
 
-        public boolean FindSong(int ID) {
-            IniciarLeituraSequencial();
+        public boolean FindSong(int ID) { // procura a musica por id
+            IniciarLeituraSequencial(); // funçao que le o ultimo id inserido
 
             int idSeacher = -1;
             try {
-                for (; idSeacher != ID || meta.lapide; raf.seek(raf.getFilePointer() + (meta.sizeBytes - 4))) { // pula
-                                                                                                                // o //
-                                                                                                                // bytes
-                                                                                                                // de
-                    // registro ate achar o ID
+                for (; idSeacher != ID || meta.lapide; raf.seek(raf.getFilePointer() + (meta.sizeBytes - 4))) { // pula o numero de bytes ate achar o registro
+            
                     this.metaSeek = raf.getFilePointer(); // salva a posição do inicio da lapide
                     meta.readMetaData(); // le os metadados
                     this.metaData = meta; // salva os metadados na classe
@@ -48,26 +47,28 @@ public class Arq {
     protected static DataInputStream dis;
     protected static MetaData meta;
 
+    // inicia o arquivo/cria o raf para gerenciar
     public static void Iniciar(String filex) {
         file = filex;
         try {
-            raf = new RandomAccessFile(file, "rw");
+            raf = new RandomAccessFile(file, "rw"); // cria pra escrita/leitura
             status = true;
             meta = new MetaData(raf);
-            Musica.setLastID(raf.readInt());
+            Musica.setLastID(raf.readInt());  // pega o ultimo id inserido
         } catch (IOException e) {
             System.out.println("Erro ao abrir o arquivo: " + e.getMessage());
         }
 
     }
 
+    // procurar a musica 
     public static Musica FindSongID(int ID) {
         Musica musica = null;
         DataFinder finder = new DataFinder();
-        if (finder.FindSong(ID)) {
+        if (finder.FindSong(ID)) { // se ele achar a musica
             try{
-                raf.seek(finder.metaSeek);
-                musica = getRegistro();
+                raf.seek(finder.metaSeek); // coloca o cabeçote no inicio do registro
+                musica = getRegistro(); // le o registro e cria o objeto
             }catch(IOException e){
                 System.out.println("Erro ao buscar a música: " + e.getMessage());
             }
@@ -76,39 +77,42 @@ public class Arq {
         return musica;
     }
 
+    // pega o status do programa
     public static boolean getStatus() {
         return status;
     }
 
+    // deleta o som pelo id
     public static boolean DeleteSong(int id) {
         boolean status = false;
         DataFinder finder = new DataFinder();
         if (finder.FindSong(id)) { //encontrou a musica a ser deletada
             try {
-                raf.seek(finder.metaSeek);
-                raf.writeBoolean(true);
+                raf.seek(finder.metaSeek); // coloca o cabeçote no inicio do registro que vai ser deletado
+                raf.writeBoolean(true); // seta a lapide como true (que deus o tenha)
                 status = true;
             } catch (IOException e) {
                 System.out.println("Erro ao deletar a música: " + e.getMessage());
             }
-        } else {
+        } else { // se nao encontrou a musica
             System.out.println("Musica não encontrada");
         }
         return status;
     }
 
+    // atualiza os dados da musica
     public static void UpdateSong(int ID, String newSong) {
         DataFinder finder = new DataFinder();
         if(finder.FindSong(ID)){
            try{
             Musica nova = Musica.StringToMusica(newSong); // cria um novo objeto com a string de parametro
-            int novaSize = nova.toByteArray().length;
+            int novaSize = nova.toByteArray().length; // pega o tamanho da string em bytes
 
-            if (novaSize <= finder.metaData.sizeBytes) { // pode sobrescrever
-                raf.seek(finder.seekSaver);
-                raf.write(nova.toByteArray());
+            if (novaSize <= finder.metaData.sizeBytes) { // pode sobrescrever (mesmo tamanho ou igual)
+                raf.seek(finder.seekSaver); // coloca o ponteiro no inicio do registro
+                raf.write(nova.toByteArray()); // sobrescreve
                 // volta para o inicio do registro (lapide)
-            } else {
+            } else { // se não puder sobrescrever (for maior)
                 raf.seek(finder.metaSeek); // volta para o inicio do registro (lapide)
                 raf.writeBoolean(true); // marca como lapide
                 addRegistroExistenteEOF(nova);// update em fim de arquivo sem alterar ultimo ID
@@ -131,17 +135,18 @@ public class Arq {
         }
     }
 
+    // le o registro
     public static Musica getRegistro() {
         try {
             Musica buffer = new Musica();
             byte[] bytes;
-            do {
+            do { 
                 meta.readMetaData();
                 bytes = new byte[meta.sizeBytes]; // Cria um array de bytes com o tamanho do registro
                 raf.readFully(bytes); // Lê o registro completo
             } while (meta.lapide && raf.getFilePointer() < raf.length()); //so retorna reg valido
 
-            buffer = Musica.fromByteArray(bytes);
+            buffer = Musica.fromByteArray(bytes); 
             return buffer;
         } catch (IOException e) {
 
@@ -151,32 +156,19 @@ public class Arq {
 
     }
 
-    public Musica getSongFromRafNow() {
-        try {
-            Musica buffer = new Musica();
-            meta.readMetaData();
-            byte[] ba = new byte[meta.sizeBytes]; // Cria um array de bytes com o tamanho do registro
-            raf.readFully(ba); // Lê o registro completo
-            buffer = Musica.fromByteArray(ba); // Converte para objeto Musica
-            return buffer;
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
 
+    // Adiciona um novo registro
     public static boolean addRegistro(String str) {
         byte[] ba;
-        Musica nova = Musica.StringToMusica(str);
-        System.out.println("ultimo id eh : " + Musica.getLastID());
-        nova.id = Musica.getLastID() + 1;
+        Musica nova = Musica.StringToMusica(str); // cria o objeto com a string
+        nova.id = Musica.getLastID() + 1; // atualiza o ultimo id
         try {
             ba = nova.toByteArray(); // atualiza lastID na classe mas não no arquivo
             Musica.setLastID(nova.getId()); // atualiza na classe
             writeLastID(Musica.getLastID()); // atualiza no arquivo
-            raf.seek(raf.length());
-            meta.writeMetaData(ba.length);
-            raf.write(ba);
+            raf.seek(raf.length()); // joga o ponteiro do RAF para o final do arquivo
+            meta.writeMetaData(ba.length); // escreve os metadados do registro
+            raf.write(ba); // escreve o registro
             return true;
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -184,12 +176,13 @@ public class Arq {
         }
     }
 
+// adiciona um registro existente
     public static boolean addRegistroExistenteEOF(Musica newsong) {
         try {
-            byte[] ba = newsong.toByteArray();
-            raf.seek(raf.length());
-            meta.writeMetaData(ba.length);
-            raf.write(ba);
+            byte[] ba = newsong.toByteArray(); // tranforma o registro em array de bytes
+            raf.seek(raf.length()); // leva o ponteiro do RAF para o final do arquivo
+            meta.writeMetaData(ba.length); // escreve o metadado do registro
+            raf.write(ba); // escreve o registro
             return true;
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -197,10 +190,11 @@ public class Arq {
         }
     }
 
+    // le o ultimo id inserido 
     public static int ReadLastID() {
         try {
-            raf.seek(0);
-            return raf.readInt();
+            raf.seek(0); // vai pro inicio do arquivo
+            return raf.readInt(); // le o id
         } catch (IOException e) {
             System.out.println("Erro ao ler o último ID: " + e.getMessage());
             return -1;
@@ -208,15 +202,17 @@ public class Arq {
 
     }
 
+    // escreve o ultimo id inserido
     public static void writeLastID(int id) {
         try {
-            raf.seek(0);
-            raf.writeInt(id);
+            raf.seek(0); // vai pro inicio do arquivo
+            raf.writeInt(id); // escreve o ultimo id
         } catch (IOException e) {
             System.out.println("Erro ao escrever o último ID: " + e.getMessage());
         }
     }
 
+    // finaliza o programa
     public static void Finalizar() {
         status = false;
         try {
